259959adfa51a6300dc006e0adbf5adcef754065
lukas <lukas@LUKAS_PC>
2023-05-31 22:22:33 +0200

    Push

---
+++ FSST Nachhilfe31.5.2023/FSST Nachhilfe31.5.2023/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace FSST_Nachhilfe31._5._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Du Goof");
+            Console.ReadLine();
+
+            // Variablen
+            bool janein = true;
+
+            char charakter = '=';
+
+            double großekomazahl = 235234534.34534890567834589698;
+
+            int Zahl = 15;
+
+            string zeichen = "ölskdfjklögklösjdfglk4390568903486(()=/(;:";
+
+            // if
+
+            if (charakter == '0')
+            {
+
+            
+            }
+            else if (Zahl == 4564)
+            {
+
+            }
+            else
+            {
+
+            }
+
+            int j = 0;
+            while (j < Zahl)
+            {
+                j++;
+            }
+
+            do
+            {
+
+            } while (j < Zahl);
+
+            for (int i = 0; i < Zahl; i++)
+            {
+                
+            }
+
+            int a = 10;
+            int b = 15;
+
+            a = b + 10;
+            a = b;
+            a += 10;
+
+            double c = Math.Sqrt(100);
+
+            int h = 1;
+            string[] Sätze = new string[30];
+
+            Sätze[0] = "349087509348";
+            Sätze[h] = "345098";
+            Sätze[2] = "345098jkglkjg";
+            Console.WriteLine($"{h+1} = {Sätze[h]}");
+
+        }
+    }
+}
