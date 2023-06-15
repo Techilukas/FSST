5314f6f9627d6e265aa03ac49ecad94593525769
Techilukas <lukas.martin@blons.net>
2023-06-15 11:27:16 +0200

    push

---
+++ Elephant_HANS/Elephant_HANS/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+
+namespace FSST_ÜBen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int count = 0;
+
+            Console.WriteLine("Wie viele Telefonnummern sollten erstellt werden");
+            bool isinputok = false;
+
+            while (!isinputok)
+            {
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out count) && count <= 100 && count > 0)
+                {
+                    isinputok = true;
+                }
+                else
+                {
+                    Console.WriteLine("Geben Sie eine Zahl ein zwischen 1 - 100");
+                }
+            }
+            Console.WriteLine();
+
+           
+
+
+            int[] telephonenumbers = new int[count];
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < telephonenumbers.Length; i++)
+            {
+                for(int j = 0; j < 8; j++)
+                {
+                    int a = rnd.Next(0, 10);
+                    if (a == 0)
+                    {
+                        telephonenumbers[i] *= 10;
+                    }
+                    else
+                    {
+                        telephonenumbers[i] += a * Convert.ToInt32(Math.Pow(10, j));
+                    }
+                }
+
+            }
+
+
+            for (int i = 0; i < telephonenumbers.Length; i++)
+            {
+                Console.WriteLine($"{i + 1,2}. 0664/{telephonenumbers[i]}");
+            }
+        }
+    }
+
+}
---
+++ Rechteck_Fsst/Rechteck_Fsst/Program.cs
@@ -0,0 +1,183 @@
+﻿using System.Net.WebSockets;
+
+namespace Rechteck_Fsst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anzahl der Rechtecke: ");
+            int count = int.Parse(Console.ReadLine());
+            int[] numbers = FillArrayRanomNumebr(count, 99, 1);
+            Console.WriteLine("Länge des Rechtecks: ");
+            int width = int.Parse(Console.ReadLine());
+            Console.WriteLine("Höhe des Rechtecks: ");
+            int height = int.Parse(Console.ReadLine());
+            DrawSquars(numbers, width, height);
+            ShowIntArray(numbers, width-2, width);
+        }
+
+        public static void DrawSquars(int[] numbers)
+        {
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                Console.Write(" ## ");
+            }
+            Console.WriteLine();
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                Console.Write(" ## ");
+            }
+            Console.WriteLine();
+        }
+
+        public static int[] FillArrayRanomNumebr(int count, int max, int min)
+        {
+            Random rnd = new Random();
+            int[] values = new int[count];
+            for (int i = 0; i < values.Length; i++)
+            {
+                values[i] = rnd.Next(min, max +1);
+            }
+            return values;
+        }
+
+        public static void ShowIntArray(int[] values, int space)
+        {
+            for (int j = 0; j < space/2; j++)
+            {
+                Console.Write($" ");
+            }
+            for (int i = 0; i < values.Length; i++)
+            {
+                Console.Write("" + values[i], 2);
+                for (int j = 0; j < space; j++)
+                {
+                    Console.Write($" ");
+                }
+
+            }
+        }
+
+        public static void ShowIntArray(int[] values, int space, int multiplicator)
+        {
+            for (int j = 0; j < space / 2; j++)
+            {
+                Console.Write($" ");
+            }
+            for (int i = 0; i < values.Length; i++)
+            {
+                Console.Write($"{values[i] * multiplicator,4}");
+                for (int j = 0; j < space; j++)
+                {
+                    Console.Write($" ");
+                }
+
+            }
+        }
+
+        public static void DrawSquars(int[] numbers, int width)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(" ");
+                for(int j = 0; j < width; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < width-2; i++)
+            {
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    Console.Write(" ");
+                    Console.Write("#");
+                    for (int l = 0; l < width-2; l++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write("#");
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(" ");
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+        public static void DrawSquars(int[] numbers, int width, int height)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(" ");
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < height - 2; i++)
+            {
+                if (i == 0)
+                {
+                    for (int j = 0; j < numbers.Length; j++)
+                    {
+                        Console.Write(" ");
+                        Console.Write("#");
+                        for (int l = 0; l < (width - 4) / 2; l++)
+                        {
+                            Console.Write(" ");
+                        }
+                        Console.Write($"{numbers[j],2}");
+                        for (int l = 0; l < (width - 4) / 2; l++)
+                        {
+                            Console.Write(" ");
+                        }
+                        Console.Write("#");
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine(); 
+                }
+                else
+                {
+                    for (int j = 0; j < numbers.Length; j++)
+                    {
+                        Console.Write(" ");
+                        Console.Write("#");
+                        for (int l = 0; l < width - 2; l++)
+                        {
+                            Console.Write(" ");
+                        }
+                        Console.Write("#");
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(" ");
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
